8af1b5f56016d703d101160c02d1c83a269a3964
dmmond <dmmond@ncsu.edu>
2023-11-07 22:46:15 -0500

    Fixing merge.

--- Project2/test/edu/ncsu/csc216/stp/model/tests/TestCaseTest.java
+++ Project2/test/edu/ncsu/csc216/stp/model/tests/TestCaseTest.java
@@ -76,9 +76,5 @@ public class TestCaseTest {
 		assertEquals("PASS", test.getStatus());
 		test.addTestResult(false, "fail again");
 		assertEquals("FAIL", test.getStatus());
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'main' of https://github.ncsu.edu/engr-csc216-fall2023/csc216-P2-T-025.git
 	}
 }
