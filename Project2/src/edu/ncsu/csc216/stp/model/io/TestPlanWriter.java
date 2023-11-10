c188e1092604af62e262433aaa852f615180bf43
gedougl2 <gedougl2@ncsu.edu>
2023-11-09 19:14:21 -0500

    [Implementation] Implementing TestPlanWriter

--- Project2/src/edu/ncsu/csc216/stp/model/io/TestPlanWriter.java
+++ Project2/src/edu/ncsu/csc216/stp/model/io/TestPlanWriter.java
@@ -2,6 +2,7 @@ package edu.ncsu.csc216.stp.model.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import edu.ncsu.csc216.stp.model.test_plans.TestPlan;
 import edu.ncsu.csc216.stp.model.util.ISortedList;
@@ -27,9 +28,23 @@ public class TestPlanWriter {
 	 */
 	public static void writeTestPlanFile(File file, ISortedList<TestPlan> list) {
 		try {
+			PrintStream fileWriter = new PrintStream(file);
+			for (int i = 0; i < list.size(); i++) {
+				TestPlan plan = list.get(i);
+				fileWriter.print("!" + plan.getTestPlanName() + "\n");
+				
+				for (int j = 0; j < plan.getTestCases().size(); j++) {
+					fileWriter.print(plan.getTestCases().get(j).toString());
+				}
+				
+				if (i != list.size() - 1) {
+					fileWriter.print("\n");
+				}
+			}
 			
+			fileWriter.close();
 		} catch (IOException e) {
-			throw new IllegalArgumentException
+			throw new IllegalArgumentException("Unable to save file.");
 		}
 	}
 }
