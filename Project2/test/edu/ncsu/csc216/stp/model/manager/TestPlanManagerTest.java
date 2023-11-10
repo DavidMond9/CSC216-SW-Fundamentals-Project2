ec4a7f6f6131dfa13cf35b26fa24a120e2234487
dmmond <dmmond@ncsu.edu>
2023-11-09 20:27:39 -0500

    [Testing] TestPlanManagerTest

--- Project2/test/edu/ncsu/csc216/stp/model/manager/TestPlanManagerTest.java
+++ Project2/test/edu/ncsu/csc216/stp/model/manager/TestPlanManagerTest.java
@@ -2,10 +2,17 @@ package edu.ncsu.csc216.stp.model.manager;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
 public class TestPlanManagerTest {
+	
+	private static TestPlanManager manager = new TestPlanManager();
+	String testFile1 = "test-plans0.txt";
 	@Test
 	void testLoadTestPlans() {
-		fail();
+		manager.loadTestPlans(new File(testFile1));
+		String[] names = manager.getTestPlanNames();
+		assertEquals("PackScheduler", names[0]);
 	}
 	@Test
 	void testSaveTestPlans() {
