7446d37a68f68b190a8220715835d2791b026873
dmmond <dmmond@ncsu.edu>
2023-11-15 00:21:04 -0500

    Fixed TestPlanManager.

--- Project2/src/edu/ncsu/csc216/stp/model/manager/TestPlanManager.java
+++ Project2/src/edu/ncsu/csc216/stp/model/manager/TestPlanManager.java
@@ -72,7 +72,7 @@ public class TestPlanManager {
 	public void addTestPlan(String testPlanName) {
 		for(int i = 0; i < testPlans.size(); i++) {
 			String currName = testPlans.get(i).getTestPlanName().toLowerCase();
-			if(currName.toLowerCase() == testPlanName.toLowerCase() || currName.toLowerCase() == failingTestList.getTestPlanName().toLowerCase() ) {
+			if(currName.equals(testPlanName.toLowerCase()) || currName.equals(failingTestList.getTestPlanName().toLowerCase()) ) {
 				throw new IllegalArgumentException("Invalid name.");
 			}
 		}
